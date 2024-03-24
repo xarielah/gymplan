4b6221bce87650278254c1d6ab95861676e757a2
Ariel <arielah1997@gmail.com>
2024-03-24 09:44:40 +0200

    fix: unused routes

--- src/app/api/plan/[id]/route.ts
+++ src/app/api/plan/[id]/route.ts
@@ -1,22 +1,22 @@
-import { dbService } from "@/db/db.service";
-import { NextResponse as res } from "next/server";
+// import { dbService } from "@/db/db.service";
+// import { NextResponse as res } from "next/server";
 
-interface PlanByIdParams {
-  params: {
-    id: string;
-  };
-}
+// interface PlanByIdParams {
+//   params: {
+//     id: string;
+//   };
+// }
 
-async function getPlanById(_: Request, { params }: PlanByIdParams) {
-  try {
-    const plan = await dbService.plan.findOne({ _id: params.id });
-    return res.json({ message: "success", plan }, { status: 200 });
-  } catch (_) {
-    return res.json(
-      { message: `Could not get a plan with id \"${params.id}\".` },
-      { status: 500 }
-    );
-  }
-}
+// async function getPlanById(_: Request, { params }: PlanByIdParams) {
+//   try {
+//     const plan = await dbService.plan.findOne({ _id: params.id });
+//     return res.json({ message: "success", plan }, { status: 200 });
+//   } catch (_) {
+//     return res.json(
+//       { message: `Could not get a plan with id \"${params.id}\".` },
+//       { status: 500 }
+//     );
+//   }
+// }
 
-export { getPlanById as GET };
+// export { getPlanById as GET };
